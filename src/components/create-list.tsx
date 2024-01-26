e9b72c23e439d5412287ef3c1ec0eb0bf680a8b6
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-01-26 19:44:00 +0500

    fix dialog close btn color

--- src/components/create-list.tsx
+++ src/components/create-list.tsx
@@ -21,9 +21,9 @@ export function CreateList() {
       </DialogTrigger>
       <DialogContent className='sm:max-w-[425px]'>
         <DialogHeader>
-          <DialogTitle>Create new list</DialogTitle>
+          <DialogTitle className='dark:text-white'>Create new list</DialogTitle>
         </DialogHeader>
-        <div className='grid gap-4 py-4'>
+        <div className='grid gap-4 py-4 dark:text-white'>
           <div className='grid grid-cols-4 items-center gap-4'>
             <Label htmlFor='list-name' className='text-right'>
               List name
